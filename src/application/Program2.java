2710d229de7206fb952490bc8598eb9696712994
Michele Freitas <michele-braga@hotmail.com>
2019-08-05 22:35:36 +0100

    Department delete implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,9 +1,7 @@
 package application;
 
-import java.util.ArrayList;
 import java.util.List;
-
-import javax.sound.midi.Soundbank;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -13,16 +11,18 @@ public class Program2 {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
 		DepartmentDao departmentdao = DaoFactory.createDepartmentDao();
 		
+		//buscar departamento por Id		
 		System.out.println("=== TESTE 1: department findById ==== ");
 		
 		Department department = departmentdao.findById(2);
 		
 		System.out.println(department);
 		
-		
-		
+		//Exibir todos departamentos
 		System.out.println("\n=== TESTE 2: department findAll ==== ");
 		
 		List<Department> list = departmentdao.findAll();
@@ -31,7 +31,7 @@ public class Program2 {
 			System.out.println(dep);
 		}
 		
-		
+		//Inserir um novo departamento
 		System.out.println("\n=== TESTE 3: department insert ==== ");
 		
 		Department depnew = new Department(null, "Music");
@@ -40,7 +40,7 @@ public class Program2 {
 		
 		System.out.println("Inserted! New id: " + depnew.getId());
 		
-		
+		//Alterar um departamento de acordo com o id escolhido do BD
 		System.out.println("\n TESTE 4: department update ==== ");
 		
 		Department dep2 = departmentdao.findById(1);
@@ -48,7 +48,17 @@ public class Program2 {
 		departmentdao.update(dep2);
 		System.out.println("Update completed!");
 		
+		//Deletar um departamento de acrodo com o id informado
+		System.out.println("\n TESTE 5: department delete ==== ");
 		
+		System.out.println("Enter id for delete test");
+		int id = sc.nextInt();
+		
+		departmentdao.deleteById(id);
+		
+		System.out.println("Delete completed!");
+		
+		sc.close();
 	}
 
 }
