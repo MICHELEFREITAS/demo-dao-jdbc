7ffadc0e47c8fc69fe423901d2ee7e0e039f276e
Michele Freitas <michele-braga@hotmail.com>
2019-08-05 19:38:27 +0100

    DepartmentDao findById and Program2

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -2,12 +2,19 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
+import model.entities.Department;
 
 public class Program2 {
 
 	public static void main(String[] args) {
 		
 		DepartmentDao departmentdao = DaoFactory.createDepartmentDao();
+		
+		System.out.println("=== TESTE 1: department findById ==== ");
+		
+		Department department = departmentdao.findById(2);
+		
+		System.out.println(department);
 
 	}
 
